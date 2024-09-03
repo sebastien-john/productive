f27dc4fce2f83638cb77f7403292c9e342fda3e1
sebastien-john <thesebastienjohn@gmail.com>
2024-09-03 02:26:44 -0400

    clock buttons and forms w/o functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import Clock from "./clock";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = createRoot(document.body);
 root.render(
--- src/clock.tsx
+++ src/clock.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 function Clock() {
-    return <h1>this is my clock</h1>;
+
+    const [timerOn, setTimerOn] = useState(false);
+
+
+  return (
+    <>
+      <input
+        type="email"
+        className="form-control"
+        id="exampleFormControlInput1"
+        placeholder="minutes"
+      ></input>
+      <button type="button" className="btn btn-primary" onClick={() => setTimerOn(!timerOn)}>
+        start focus session
+      </button>
+      <button type="button" className="btn btn-primary" onClick={() => console.log(timerOn)}>+</button>
+      <button type="button" className="btn btn-primary">-</button>
+    </>
+  );
 }
 
 export default Clock;
