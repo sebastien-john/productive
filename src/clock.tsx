59c7b2c5ce21c28d87c5f8b0124d04abc08d03e9
sebastien-john <thesebastienjohn@gmail.com>
2024-09-04 21:17:29 -0400

    count down + progress bar functionality

--- src/clock.tsx
+++ src/clock.tsx
@@ -1,23 +1,92 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 function Clock() {
+  const [timerOn, setTimerOn] = useState(false);
+  const [totalMinutes, setTotalMinutes] = useState(5);
+  const [minutesRemaining, setMinutesRemaining] = useState(0);
 
-    const [timerOn, setTimerOn] = useState(false);
+  function createDivStyle(customWidth:number) {
+    const divStyle = {
+      width: customWidth + "%",
+    };
+    return divStyle
+  }
 
+  useEffect(() => {
+    let intervalId: string | number | NodeJS.Timeout | undefined;
+    if (timerOn) {
+      console.log(minutesRemaining);
+      intervalId = setInterval(() => setMinutesRemaining(minutesRemaining - 1), 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [timerOn, minutesRemaining]);
+
+  // TODO: Add a totalMinutes state, make it so that you can set the minutes remaining by clicking start focus session
+  // TODO: Make it so that when you click start focus session, the screen changes so that you can see a progress bar of sorts
+  // ... instead of the input field, conditional rendering.
+  // TODO: additional logic to increment/decrement totalMinutes by clicking + or -, and having a max and min number of minutes
+  // TODO: conditional rendering of the buttons so that you only see the stop button while the timer is going
+  // TODO: more sophisticated UI, a more fitting input field, could try to replicate some of the smoothness of the windows app
 
   return (
     <>
-      <input
-        type="email"
-        className="form-control"
-        id="exampleFormControlInput1"
-        placeholder="minutes"
-      ></input>
-      <button type="button" className="btn btn-primary" onClick={() => setTimerOn(!timerOn)}>
-        start focus session
-      </button>
-      <button type="button" className="btn btn-primary" onClick={() => console.log(timerOn)}>+</button>
-      <button type="button" className="btn btn-primary">-</button>
+      {timerOn === false ? (
+        <>
+          <h1> {totalMinutes} </h1>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => {
+              setTimerOn(!timerOn);
+              setMinutesRemaining(totalMinutes);
+
+              console.log(totalMinutes);
+            }}
+          >
+            start focus session
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => setTotalMinutes(totalMinutes + 5)}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => setTotalMinutes(Math.max(5, totalMinutes - 5))}
+          >
+            -
+          </button>
+        </>
+      ) : (
+        <>
+          <div
+            className="progress"
+            role="progressbar"
+            aria-label="Example with label"
+            aria-valuenow={50}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          >
+            <div className="progress-bar" style={createDivStyle(100*minutesRemaining/totalMinutes)}>
+              {minutesRemaining} minutes remaining
+            </div>
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => {
+              setTimerOn(!timerOn);
+              setMinutesRemaining(totalMinutes);
+            }}
+          >
+            restart focus session
+          </button>
+        </>
+      )}
     </>
   );
 }
